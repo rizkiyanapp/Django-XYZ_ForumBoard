1459d8e4d324f0083284dbb67080b5fb7fbd9fe1
rizkiyanapp <rizkiyanapp@gmail.com>
2018-06-17 23:38:41 -0700

    Add: Bootstrap, first home view, home test case and admin site

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Board
+
 def home(request):
-    return HttpResponse('Hi, glad to be back my King!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards':boards})
